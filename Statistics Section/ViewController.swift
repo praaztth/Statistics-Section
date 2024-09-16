0d84c9c953efa795424d6ab5acde7af36d1c3467
tryuruy <i-cho-ter@mail.ru>
2024-09-16 18:49:14 +0300

    Initial Commit

---
+++ Statistics Section/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Statistics Section
+//
+//  Created by tryuruy on 16.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
